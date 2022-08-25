e15eddc60dadc819766336ccb3b05581aa033df1
marques-j-robinson <marques.j.robinson@gmail.com>
2022-08-25 15:48:31 -0700

    uncomment solution templates code

--- solution_template.py
+++ solution_template.py
@@ -1,20 +1,19 @@
 from util.base_solution import BaseSolution
 
 
-# class Solution(BaseSolution):
+class Solution(BaseSolution):
 
-    # def __init__(self):
-        # self.year = EVENT
-        # self.day = DAY
+    def __init__(self):
+        self.year = EVENT
+        self.day = DAY
 
-    # def part_01(self):
-        # pass
+    def part_01(self):
+        pass
 
-    # def part_02(self):
-        # pass
+    def part_02(self):
+        pass
 
 
 if __name__ == '__main__':
-    pass
-    # s = Solution()
-    # s.solve()
+    s = Solution()
+    s.solve()
