0e7b9b2dce64af65ee38c324b2a937a7ce53fd51
marques-j-robinson <marques.j.robinson@gmail.com>
2022-09-03 20:48:50 -0700

    solve 2016 day 3 part 2

--- puzzles/2016_03.py
+++ puzzles/2016_03.py
@@ -1,8 +1,13 @@
 from util.base_solution import BaseSolution
 
 
-def compare(a, b, c):
+def compare(lengths):
+    [a, b, c] = [int(i) for i in lengths]
     return a+b>c and a+c>b and b+c>a
+
+
+def should_reset(val):
+    return val % 3 == 0
 
 
 class Solution(BaseSolution):
@@ -10,19 +15,38 @@ class Solution(BaseSolution):
     def __init__(self):
         self.year = 2016
         self.day = 3
+        self.tri_a = []
+        self.tri_b = []
+        self.tri_c = []
+
+    def reset_triangles(self):
+        self.tri_a = []
+        self.tri_b = []
+        self.tri_c = []
 
     def translate(self):
         self.split_by_new_line()
-        self.data = [[int(i) for i in l.strip().split()] for l in self.data]
+        self.data = [l.strip().split() for l in self.data]
 
     def part_01(self):
         for l in self.data:
-            [a, b, c] = l
-            if compare(a, b,c) is True:
+            if compare(l) is True:
                 self.p1 += 1
 
     def part_02(self):
-        pass
+        for idx, l in enumerate(self.data):
+            [a, b, c] = l
+            self.tri_a.append(a)
+            self.tri_b.append(b)
+            self.tri_c.append(c)
+            if should_reset(idx + 1):
+                if compare(self.tri_a) is True:
+                    self.p2 += 1
+                if compare(self.tri_b) is True:
+                    self.p2 += 1
+                if compare(self.tri_c) is True:
+                    self.p2 += 1
+                self.reset_triangles()
 
 
 if __name__ == '__main__':
